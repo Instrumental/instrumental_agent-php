2c5603197c8b85358905586fe9dad986c66fa4a3
Jason Gladish <jason@expectedbehavior.com>
2016-05-03 18:57:44 -0400

    Lowered amount of data sent through disconnected pipe so there isn't quite so much output or time taken.

--- test/AgentTest.php
+++ test/AgentTest.php
@@ -49,10 +49,10 @@ class AgentTest extends \PHPUnit_Framework_TestCase
         sleep(2);
 
         // Send enough through the socket that we can tell we're disconnected.
-        for($i=1; $i<=1000; ++$i) {
+        for($i=1; $i<=200; ++$i) {
           $ret = $I->increment('php.increment', $i);
         }
-        $this->assertEquals(1000, $ret);
+        $this->assertEquals(200, $ret);
 
         $this->assertRegExp($expectedData, file_get_contents("test/server_commands_received"));
 
